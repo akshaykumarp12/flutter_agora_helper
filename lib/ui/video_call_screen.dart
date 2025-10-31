52c4f11005e61fefe659b74bcb86d475296ebb04
akshaykumarp12 <akshay.kumar@lilacinfotech.com>
2025-10-31 12:36:59 +0530

    updates in conroller

--- lib/ui/video_call_screen.dart
+++ lib/ui/video_call_screen.dart
@@ -395,6 +395,8 @@ class _VideoCallScreenState extends ConsumerState<VideoCallScreen> {
           }
           final user = ref.watch(remoteUser);
           if (user == null) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Error joining video call")));
             return child!;
           }
           return AgoraVideoView(
